7b51f2e6e84d836045ce65d8c725e8d2534b47d9
Nathan Hill <nathan.hill@inin.com>
2016-12-28 16:31:52 -0500

    Added convertReferenceToUint8Array.

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.convertNSArrayToArray = convertNSArrayToArray;
+exports.convertReferenceToUint8Array = convertReferenceToUint8Array;
+/* globals interop */
 
 /**
 * Converts an NSArray from the Objective-C runtime to a JavaScript array.
@@ -20,4 +22,32 @@ function convertNSArrayToArray(nsarray) {
     }
 
     return array;
+}
+
+/**
+* Converts a reference to an array of bytes to a Uint8Array.
+*
+* @param   {interop.Reference} reference
+* @param   {int}               length
+* @returns {Uint8Array}
+*/
+function convertReferenceToUint8Array(reference, length) {
+
+    if (!(reference instanceof interop.Reference)) {
+        throw new Error('The first parameter must be an interop.Reference instance.');
+    }
+    if (!Number.isInteger(length)) {
+        throw new Error('The second parameter must be an integer.');
+    }
+
+    var uint8Array = new Uint8Array(length);
+
+    for (var byteIndex = 0; byteIndex < length; byteIndex++) {
+
+        var bytePointer = reference.add(byteIndex),
+            byteReference = new interop.Reference(interop.types.uint8, bytePointer);
+
+        uint8Array[byteIndex] = byteReference.value;
+    }
+    return uint8Array;
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* globals interop */
 
 /**
 * Converts an NSArray from the Objective-C runtime to a JavaScript array.
@@ -14,4 +15,32 @@ export function convertNSArrayToArray(nsarray) {
     }
 
     return array;
+}
+
+/**
+* Converts a reference to an array of bytes to a Uint8Array.
+*
+* @param   {interop.Reference} reference
+* @param   {int}               length
+* @returns {Uint8Array}
+*/
+export function convertReferenceToUint8Array(reference, length) {
+
+    if (!(reference instanceof interop.Reference)) {
+        throw new Error('The first parameter must be an interop.Reference instance.');
+    }
+    if (!Number.isInteger(length)) {
+        throw new Error ('The second parameter must be an integer.');
+    }
+
+    let uint8Array = new Uint8Array(length);
+
+    for (let byteIndex = 0; byteIndex < length; byteIndex++) {
+
+        let bytePointer = reference.add(byteIndex),
+            byteReference = new interop.Reference(interop.types.uint8, bytePointer);
+
+        uint8Array[byteIndex] = byteReference.value;
+    }
+    return uint8Array;
 }
